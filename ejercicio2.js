a87ead8125d42541afa20f017efacc8aae946148
jais210 <jakeline_210@hotmail.com>
2017-07-15 12:42:57 -0500

    modificación

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,15 +1,15 @@
 // Ejercicio para hallar el número Pi
 // 1.ejercicio con variables
-var limite = 150;
+var limite = 10;
+var resultado=0;
 for (var i = 0; i < limite; i++) {
   var impares = 2 * i + 1;
-  var resultado;
+  
 
   if (i % 2 == 0) {
     resultado = resultado + 1 / impares;
   } else {
     resultado = resultado - 1 / impares;
   }
+  console.log(4 * resultado);
 }
-
-console.log(4 * resultado);
